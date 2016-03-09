1ecf5ee8877c869cb1f48bb9238cb59c476bd703
Alonso Castellon <Alonso.B.Castellon.-ND@disney.com>
2016-03-09 17:58:35 -0500

    test FileDownloader

--- src/test/java/com/osclass/test/ImageTest.java
+++ src/test/java/com/osclass/test/ImageTest.java
@@ -1,27 +1,56 @@
 package com.osclass.test;
 
+import java.io.File;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import com.osclass.utils.FileDownloader;
 import com.osclass.utils.URLStatusChecker;
 import com.osclass.utils.URLStatusChecker.RequestMethod;
 
 public class ImageTest {
 	URLStatusChecker urlChecker;
+	FileDownloader downloadTestFile;
+	WebDriver driver = new FirefoxDriver();
 	
 	@BeforeTest
     public void setup() throws Exception {
-    	WebDriver driver = new FirefoxDriver();
     	urlChecker = new URLStatusChecker(driver);
+    	//downloadTestFile = new FileDownloader(driver);
     }
 	
 	@Test
 	public void statusCode404FromString() throws Exception {
-	    urlChecker.setURIToCheck("http://www.morrisvende.com/doesNotExist.html");
+	    urlChecker.setURIToCheck("http://anunciamex.mx/items/images/ffUnbBSsuh/file5.png");
 	    urlChecker.setHTTPRequestMethod(RequestMethod.GET);
 	    Assert.assertEquals(urlChecker.getHTTPStatusCode(), 404);
+	}
+	
+	
+	public void downloadAFile() throws Exception {
+	    FileDownloader downloadTestFile = new FileDownloader(driver);
+	    driver.get("http://www.localhost.com/downloadTest.html");
+	    WebElement downloadLink = driver.findElement(By.id("fileToDownload"));
+	    String downloadedFileAbsoluteLocation = downloadTestFile.downloadFile(downloadLink);
+	    
+	    Assert.assertEquals(new File(downloadedFileAbsoluteLocation).exists(), true);
+	    Assert.assertEquals(downloadTestFile.getHTTPStatusOfLastDownloadAttempt(), 200);
+	}
+	 
+	
+	public void downloadAnImage() throws Exception {
+	    String urlPath = "http://anunciamex.mx/items/images/ffUnbBSsuh/file1.png";
+	    String downloadedImageAbsoluteLocation = downloadTestFile.downloadImage(urlPath);
+	    
+	    Assert.assertEquals(new File(downloadedImageAbsoluteLocation).exists(), true);
+	    Assert.assertEquals(downloadTestFile.getHTTPStatusOfLastDownloadAttempt(), 200);
+
 	}
 	
 }
